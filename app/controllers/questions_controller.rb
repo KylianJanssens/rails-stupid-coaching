eee79f1bf4ee2bde686fbe09e5c3e9027bfc74a9
Kylian Janssens <kylianjanssens@hotmail.com>
2023-10-02 11:52:46 +0200

    completed stupid coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,21 @@
 class QuestionsController < ApplicationController
   def ask
+
   end
 
   def answer
-  @question = params[:question]
-  if @question.blank?
-    @answer = "I can't hear you"
-  elsif @question =~ /i am going to work/i # this is to avoid double work for capital letters..
-    @answer = "Great!"
-  elsif @question.ends_with?("?")
-    @answer = "Silly question, get dressed and go to work!"
-  else
-    @answer = "I don't care, get dressed and go to work!"
-  end
+    @question = params[:question]
+    if @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question.blank?
+      @answer = "I can't hear you!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
+# If the message is I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise the coach will answer I don't care, get dressed and go to work!
